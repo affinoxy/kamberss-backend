f045474407d68601bc260ec6562aafa97f616f52
Affinoxy <ichsanhimawan19@gmail.com>
2025-12-19 00:52:56 +0700

    Fix CORS for Railway 6

--- server.js
+++ server.js
@@ -21,17 +21,13 @@ app.use(express.json())
 // PostgreSQL Connection
 // ======================
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false
-  },
-  family: 4 // ⬅️ PAKSA IPv4 (INI KUNCINYA)
+  }
 })
+
 
 
 
